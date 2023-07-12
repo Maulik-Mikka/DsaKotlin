8833711fe33e56fb149e8767d23b5f9b4f553097
MaulikMikka <61727670+MaulikMikka@users.noreply.github.com>
2023-07-11 22:37:32 -0600

    VowelRemove

--- app/src/main/java/com/practical/kotlinconcepts/Easy/Strings/VowelsRemove.kt
+++ app/src/main/java/com/practical/kotlinconcepts/Easy/Strings/VowelsRemove.kt
@@ -1,6 +1,19 @@
 package com.practical.kotlinconcepts.Easy.Strings
 
-fun main()
-{
+fun main() {
 
+    removeVowels("leetcodeisacommunityforcoders")
+}
+
+
+fun removeVowels(s: String): String {
+    var result = ""
+    for (i in s.indices) {
+        if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') {
+        } else {
+            result += s[i]
+        }
+    }
+
+    return result
 }
